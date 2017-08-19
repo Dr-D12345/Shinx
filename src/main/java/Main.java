fe39af4fdb7e317b96fabf42842e07a666ecd806
Marko <jvilanyu@gmail.com>
2017-08-19 15:07:36 -0400

    hello

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,16 @@
+import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
 
 /**
  * Created by derekpastor on 8/19/17.
  */
 
 public class Main {
-    public static void main(String args[]){
-        JDA jda = new JDABuilder(AccountType.BOT).setToken("token").buildBlocking();
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
+    public static void main(String args[]) throws LoginException, RateLimitedException, InterruptedException{
+        JDA jda = new JDABuilder(AccountType.BOT).setToken("MzQ4NTI3MTAwMzE3NDAxMDk5.DHoPWw.VW5q4zAFUjLbdC1H8sC2GXjtGDA").buildBlocking();
     }
 }
