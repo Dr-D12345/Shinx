3f227ce5fdc1d96fe7edd43887c17a219902ecbd
Marko <jvilanyu@gmail.com>
2017-08-19 14:38:44 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,13 @@ import net.dv8tion.jda.core.JDA;
 /**
  * Created by derekpastor on 8/19/17.
  */
+
 public class Main {
     public static void main(String args[]){
         JDA jda = new JDABuilder(AccountType.BOT).setToken("token").buildBlocking();
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     }
 }
